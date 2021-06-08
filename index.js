9d4bcd5da048432c2a5e0466fd9f4f77d12f1ae9
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-06-08 17:40:16 +0200

    exo fini

---
+++ index.js
@@ -0,0 +1,61 @@
+// Sélectionner nos éléments
+let input = document.querySelector('#prix');
+let error = document.querySelector('small');
+let formulaire = document.querySelector('#formulaire');
+
+
+// Cacher l'erreur
+error.style.display = 'none';
+
+// Générer un nombre aléatoire
+let nombreAleatoire = Math.floor(Math.random() * 1001);
+let tentatives = 0;
+let nombreSaisie;
+
+// Créer la fonction vérifier
+function verifier(chiffre) {
+
+    let instruction = document.createElement('div');
+     
+    if(chiffre < nombreAleatoire){
+        instruction.className = 'moins';
+        instruction.textContent = 'C\'est plus de ' + chiffre;
+        formulaire.append(instruction);
+
+    }else if(chiffre > nombreAleatoire){
+        instruction.classList = 'plus';
+        instruction.textContent = 'C\'est moins de ' + chiffre;
+        formulaire.append(instruction);
+
+    }else{
+        instruction.classList = 'fini';
+        instruction.textContent = 'Bravo vous avez trouvé le juste prix qui est :' + nombreAleatoire + ' en ' + tentatives + ' tentatives';
+        formulaire.append(instruction);
+    }
+}
+
+// Vérifier que l'utilisateur donne bien un nombre
+input.addEventListener('keyup', () => {
+    if(isNaN(input.value) || input.value >= 1001 || input.value < 0 || !input.value){
+        error.style.display = 'inline';
+    }else{
+        error.style.display = 'none';
+
+    }
+});
+
+// Agir à l'envoi du formulaire
+formulaire.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    if(isNaN(input.value) || input.value >= 1001 || input.value < 0 ){
+        input.style.borderColor = "red";
+    }else{
+        tentatives++;
+        input.style.borderColor = "green";
+        nombreSaisie = input.value;
+        input.value = '';
+        verifier(nombreSaisie);
+    }
+})
+
